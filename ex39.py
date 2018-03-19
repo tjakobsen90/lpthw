c136e79e5c4e2e884c9dfc2001ecace6a94577a7
tjakobsen90 <tjakobsen@live.nl>
2018-03-19 21:51:25 +0100

    add ex39 full

--- ex39.py
+++ ex39.py
@@ -19,3 +19,39 @@ cities['NY'] = 'New York'
 cities['OR'] = 'Portland'
 
 # print out some cities
+print '-' * 10
+print "NY State has: ", cities['NY']
+print "OR State has: ", cities['OR']
+
+# print some states
+print '-' * 10
+print "Michigan's abbrevation is: ", states['Michigan']
+print "Florida's abbreviation is: ", states['Florida']
+
+# do it by using the state then the cities dict
+print '-' * 10
+print "Michigan has: ", cities[states['Michigan']]
+print "Florida has: " , cities[states['Florida']]
+
+# print every state abbreviation
+print '-' * 10
+for state, abbrev in states.items():
+	print "%s is abbreviated %s" % (state, abbrev)
+
+# print every city in state
+print '-' * 10
+for abbrev, city in cities.items():
+	print "%s has the city %s" % (abbrev, city)
+
+# now do both at the same time
+print '-' * 10
+for state, abbrev in states.items():
+	print "%s state is abbreviation by state that might not be there
+	state = states.get('Texas', None)
+
+if not state:
+	print "Sorry, no Texas."
+
+# get a city with a default value
+city = cities.get('TX', 'Does not exist')
+print "The city for the state 'TX' is: %s" % city
