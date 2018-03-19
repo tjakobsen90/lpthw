bdf40b0d47520997f6ed8c71f7d1d7b37bb87041
tjakobsen90 <tjakobsen@live.nl>
2018-03-19 21:55:07 +0100

    addd ex39 with fix

--- ex39.py
+++ ex39.py
@@ -46,8 +46,12 @@ for abbrev, city in cities.items():
 # now do both at the same time
 print '-' * 10
 for state, abbrev in states.items():
-	print "%s state is abbreviation by state that might not be there
-	state = states.get('Texas', None)
+	print "%s state is abbreviation %s and has city %s" % ( 
+	state, abbrev, cities[abbrev])
+
+print '-' * 10
+#safely get an abbreviation by state that might not be there
+state = states.get('Texas', None)
 
 if not state:
 	print "Sorry, no Texas."
